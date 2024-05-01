c699c398ecdfc3984f69ee954b7d2730f581ae89
HeDubing <105642943+hdb123456@users.noreply.github.com>
2024-05-01 21:58:30 +0800

    Update fifo.c

--- concurrent_programming/FIFO/fifo.c
+++ concurrent_programming/FIFO/fifo.c
@@ -3,7 +3,13 @@
 
 int main(int argv ,char **argc)
 {
-    mkfifo("./Ffifo","0777")
+    //检测是否存在管道(未实现)并mkfifo创建管道
+   if( fifo=mkfifo("./Ffifo","777")==0)
+   {
+        perror("fifo file create success\n");
+   }
+    
+    
 
   
 }
