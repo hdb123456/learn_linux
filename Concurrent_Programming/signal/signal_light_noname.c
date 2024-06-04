93076bbab25e85e22bb1cc20b696bc14488d998f
hdb123456 <2755631915@qq.com>
2024-06-05 00:06:17 +0800

    Create signal_light_noname.c

---
+++ Concurrent_Programming/signal/signal_light_noname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*ftok
+
+
+
+,shmget,shmat,semopen_rsemopen_w,semwait,fgets,sem_post*/
+
+
+
+int main()
+{   
+    sem_init();
+
+    sem_wait();
+    sem_post();
+
+    sem_destroy();
+
+
+}
