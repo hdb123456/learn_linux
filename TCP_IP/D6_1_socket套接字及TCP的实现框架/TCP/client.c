264f9c84bbfbdcd07b66b5824fa7e176af0a5cd4
xiaohe <2755631915@qq.com>
2024-06-24 17:18:36 +0800

    end

--- TCP_IP/D6_1_socket套接字及TCP的实现框架/TCP/client.c
+++ TCP_IP/D6_1_socket套接字及TCP的实现框架/TCP/client.c
@@ -4,35 +4,30 @@
 #include "stdio.h"
 #include "unistd.h"
 #include "string.h"
+#include "stdlib.h"
+#include "errno.h"
+
 /*define area start*/
 #define DEBUG 0
 /*define area end*/
 
 /*structure area start*/
-struct sockaddr_in{
-    sa_family_t sin_family;
-    uint16_t sin_port;
-    struct in_addr sin_addr;
-};
-struct in_addr{
-    uint32_t s_addr;
-};
 /*structure area end*/
 int main (int argc, char* argv[])
 {
     if(argc != 3){
         printf("Usage: %s <ip> <port>\n",argv[0]);
-        return exit(EXIT_FAILURE);
+        exit(EXIT_FAILURE);
     }
     /*create socket*/
-    int sockfd = socket(AF_INET,SOCK_STREAM,0)
-    if(socket_fd == -1)
+    int sockfd = socket(AF_INET,SOCK_STREAM,0);
+    if(sockfd == -1)
     {
-        fprintf(stderr,"socket ERROR:%s\n",strrerror(error));
+        fprintf(stderr,"socket ERROR:%s\n",strerror(errno));
         exit(EXIT_FAILURE);
     }
     /*bind socket*/
-    struct socketaddr_in addr;
+    struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_port = htons(atoi(argv[2]));//将主机字节序转为网络字节序大端,头文件为#include "arpa/inet.h"
 #if DEBUG
@@ -45,25 +40,25 @@ int main (int argc, char* argv[])
         printf("error inet_aton\n");
         exit(EXIT_FAILURE);
     }
-    ret_connect = connect(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+   int ret_connect = connect(sockfd, (struct sockaddr *)&addr, sizeof(addr));
     if( ret_connect == -1)
     {
-        fprintf(stderr,"bind ERROR:%s\n",strrerror(error));
+        fprintf(stderr,"bind ERROR:%s\n",strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-    buf[BUFSIZ]={0};
+    char buf[BUFSIZ]={0};
     while(1)
     {
         printf("please input:\n");
         fgets(buf,BUFSIZ,stdin);
-        ret_write = write(sockfd,buf,strlen(buf));
+        int ret_write = write(sockfd,buf,strlen(buf));
         if(ret_write == -1)
         {
-            fprintf(stderr,"write ERROR:%s\n",strrerror(error));
+            fprintf(stderr,"write ERROR:%s\n",strerror(errno));
             exit(EXIT_FAILURE);
         }
     }
  
-    close(fd);
+    close(sockfd);
 }
--- TCP_IP/D6_1_socket套接字及TCP的实现框架/TCP/server.c
+++ TCP_IP/D6_1_socket套接字及TCP的实现框架/TCP/server.c
@@ -4,15 +4,9 @@
 #include "stdio.h"
 #include "unistd.h"
 #include "string.h"
+#include "stdlib.h"
+#include "errno.h"
 
-struct sockaddr_in{
-    sa_family_t sin_family;
-    uint16_t sin_port;
-    struct in_addr sin_addr;
-};
-struct in_addr{
-    uint32_t s_addr;
-};
 int main (int argc, char* argv[])
 {
     if(argc != 3)
@@ -21,14 +15,14 @@ int main (int argc, char* argv[])
         exit(EXIT_FAILURE);
     }
     /*create socket*/
-    int sockfd = socket(AF_INET,SOCK_STREAM,0)
-    if(socket_fd == -1)
+    int sockfd = socket(AF_INET,SOCK_STREAM,0);
+    if(sockfd == -1)
     {
-        fprintf(stderr,"socket ERROR:%s\n",strrerror(error));
+        fprintf(stderr,"socket ERROR:%s\n",strerror(errno));
         exit(EXIT_FAILURE);
     }
     /*bind socket*/
-    struct socketaddr_in addr;
+    struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_port = htons(atoi(argv[2]));//将主机字节序转为网络字节序大端,头文件为#include "arpa/inet.h"
     //addr.sin_addr.s_addr = argv[0];//INADDR_ANY是一个预定义的常量，其值通常为0，表示套接字将绑定到本机的所有IP地址上
@@ -38,17 +32,17 @@ int main (int argc, char* argv[])
         printf("error inet_aton\n");
         exit(EXIT_FAILURE);
     }
-    ret_bind = bind(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+    int ret_bind = bind(sockfd, (struct sockaddr *)&addr, sizeof(addr));
     if(ret_bind == -1)
     {
-        fprintf(stderr,"bind ERROR:%s\n",strrerror(error));
+        fprintf(stderr,"bind ERROR:%s\n",strerror(errno));
         exit(EXIT_FAILURE);
     }
     /*listen socket*/
-    listen_ret = listen(sockfd,5);
+    int listen_ret = listen(sockfd,5);
     if(listen_ret == -1)
     {
-        fprintf(stderr,"listen ERROR:%s\n",strrerror(error));
+        fprintf(stderr,"listen ERROR:%s\n",strerror(errno));
         exit(EXIT_FAILURE);
     }
     /* accept client */
@@ -56,7 +50,7 @@ int main (int argc, char* argv[])
     socklen_t len = sizeof(client_addr);
     int fd = accept(sockfd, (struct sockaddr *)&client_addr, &len);
     if(fd==-1){
-        fprintf(stderr,"accept ERROR:%s\n",strreror(error));
+        fprintf(stderr,"accept ERROR:%s\n",strerror(errno));
         exit(EXIT_FAILURE);
     }
     char buf[BUFSIZ]={0};
@@ -65,11 +59,11 @@ int main (int argc, char* argv[])
         memset(buf,0,BUFSIZ);
         ssize_t bytes_read = read(fd,buf,BUFSIZ);
         if(bytes_read==-1){
-            fprintf(stderr,"read ERROR:%s\n",strrerror(error));
+            fprintf(stderr,"read ERROR:%s\n",strerror(errno));
             exit(EXIT_FAILURE);
         }
         else{
-            printf("recv:%s\n",buf);
+            printf("recv:%s",buf);
         }
     }
     close(fd);
